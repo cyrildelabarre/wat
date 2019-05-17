b5b7e4714d617e02a2f56b2979f49d5c579ef2e3
Cyril Delabarre <cyril.delabarre@hotmail.com>
2019-05-17 11:31:22 +0200

    maj articles.js

--- client/src/components/Articles.js
+++ client/src/components/Articles.js
@@ -14,7 +14,8 @@ class Articles extends Component {
     this.props.getArticles()
 
     this.state = {
-      displayIframe: false
+      displayIframe: false,
+      imageLoadError: true,
     }
   }
 
@@ -28,13 +29,37 @@ class Articles extends Component {
     this.setState({
       displayIframe: !this.state.displayIframe
     })
+     window.scrollTo(0, 0)
+  }
+
+  onError() {
+    this.setState({
+      imageUrl: "https://media.giphy.com/media/uprwwjptZW4Za/giphy.gif"
+    })
+  }
+
+
+  closeIframe() {
+    let element = document.getElementById('myId');
+
+      element.classList.add('hideIframe');
+      element.classList.remove("showIframe");
   }
 
   render () {
     return (
       // {TOP 2 ARTICLES}
       <div>
-        <Iframe src='' id="myId" className={this.state.displayIframe ? "myClassname showIframe" : "myClassname"} display="initial" position="relative" allowFullScreen />
+        <div id="mainIframe" className={this.state.displayIframe ? "myClassname showIframe" : "myClassname"}>
+          <div id="iframeControl" onClick={this.closeIframe} className={this.state.displayIframe ? "myClassname showIframe" : "myClassname"}>
+            <p>Retour sur Wat WAT ?</p>
+            <button type="button" className="close" aria-label="Close">
+              <span aria-hidden="true">×</span>
+            </button>
+
+          </div>
+          <Iframe src='' id="myId" display="initial" position="relative" allowFullScreen />
+        </div>
         <MDBRow className="m-top-5 px-5 mx-auto justify-content-center"
           style={{ fontWeight: 300, maxWidth: "100%" }}
           >
@@ -67,56 +92,60 @@ class Articles extends Component {
                       var timeDiff = (now - publishedTime)
                       var time = new Date(timeDiff).toLocaleTimeString("fr-FR")
                       const timePassed = time.split(':')[0] + 'h';
-
                       if (i < 2) {
+                        var birthdays = require('celeb-birthdays');
+                        console.log(birthdays['03-13']);
                         return (
                           <MDBCol lg="6" md="12" key={i} className="d-flex" onClick={this.openIframe.bind(this, article.url)}>
-                              <div style={{
-                                  marginBottom: "1.5rem"
-                                }}>
-                                <MDBView hover rounded className="z-depth-1-half mb-4">
-                                  <img
-                                    height={200}
-                                    className="top-img"
-                                    src={article.urlToImage}
-                                    alt=""
-                                    />
+                            <div style={{
+                                marginBottom: "1.5rem"
+                              }}>
+                              <MDBView hover rounded className="z-depth-1-half mb-4">
+                                <img
+                                  height={200}
+                                  className="top-img"
+                                  onError={(e)=>{ if (e.target.src !== "https://media.giphy.com/media/uprwwjptZW4Za/giphy.gif"){ e.target.onerror = null; e.target.src="https://media.giphy.com/media/uprwwjptZW4Za/giphy.gif"; } }}
+                                  src={article.urlToImage}
+                                  alt=""
+                                  />
 
-                                  <MDBMask overlay="white-slight" className="waves-light" />
+                                <MDBMask overlay="white-slight" className="waves-light" />
 
-                                </MDBView>
-                                <div className="d-flex justify-content-between">
+                              </MDBView>
+                              <div className="d-flex justify-content-between">
 
-                                  <h6 className="">
-                                    <MDBIcon icon="plane" className="pr-2" />
-                                    {article.source.name}
-                                  </h6>
+                                <h6 className="">
+                                  <MDBIcon icon="plane" className="pr-2" />
+                                  {article.source.name}
+                                </h6>
 
-                                  <p className="font-weight-bold dark-grey-text">
-                                    <MDBIcon far icon="clock" className="pr-2" />
-                                    {timePassed}
-                                  </p>
-                                </div>
-                                <h3 className="font-weight-bold dark-grey-text mb-3 p-0">
-                                  {article.title}
-                                </h3>
-                                <p className="dark-grey-text">
-                                  {article.description}
+                                <p className="font-weight-bold dark-grey-text">
+                                  <MDBIcon far icon="clock" className="pr-2" />
+                                  {timePassed}
                                 </p>
                               </div>
+                              <h3 className="font-weight-bold dark-grey-text mb-3 p-0">
+                                {article.title}
+                              </h3>
+                              <p className="dark-grey-text">
+                                {article.description}
+                              </p>
+                            </div>
                           </MDBCol>
                         )
                       } else {
                         return (
                           // {REST OF ARTICLES}
-                          <ul className="list-unstyled" key={i}>
-                            <a href={article.url} target="_blank" rel="noopener noreferrer">
+                          <ul className="list-unstyled" key={i} onClick={this.openIframe.bind(this, article.url)}>
+
                               <Media as="li" key={i}>
                                 <img
                                   width={264}
                                   height={180}
                                   className="mr-3"
+
                                   src={article.urlToImage}
+                                  onError={(e)=>{ if (e.target.src !== "https://media.giphy.com/media/uprwwjptZW4Za/giphy.gif"){ e.target.onerror = null; e.target.src="https://media.giphy.com/media/uprwwjptZW4Za/giphy.gif"; } }}
                                   alt="Generic placeholder"
                                   />
                                 <Media.Body>
@@ -127,7 +156,7 @@ class Articles extends Component {
                                   <p>{timePassed}</p>
                                 </Media.Body>
                               </Media>
-                            </a>
+
                           </ul>
                         )
                       }
